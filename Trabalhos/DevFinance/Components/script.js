87d72603ef5a9df8a5bc193d049134f9985b09d3
LeonardoTumani <105797191+LeonardoTumani@users.noreply.github.com>
2023-05-04 08:48:51 -0300

    Update script.js

--- Trabalhos/DevFinance/Components/script.js
+++ Trabalhos/DevFinance/Components/script.js
@@ -146,7 +146,7 @@ const Utils = {
 
     formatAmount: (value) => {
         value = Number(value.replace(/\,\./g, '')) * 100;
-        return value;
+        return Math.round(value);
     },
 
     formatDate: (date) => {
